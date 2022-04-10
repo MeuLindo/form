6f321169f3e325d9aa7eb9d484b489620b0d48ac
Alexandre F <alexandreffaria@me.com>
2022-04-10 17:46:39 -0300

    radio and list selection

--- src/Form.js
+++ src/Form.js
@@ -5,20 +5,30 @@ export default function Form() {
         firstName: "",
         lastName: "",
         email: "",
-        comments: ""
+        comments: "",
+        isFriendly: true,
+        employment: "",
+        favColor: "",
     })
     console.log(formData)    
    
     function handleChange(event){
-
+        const {name, value, type, checked} = event.target
         setFormData(prevFormData => ({
             ...prevFormData,
-            [event.target.name]: event.target.value
+            [name]: type === 'checkbox'? checked : value
             }))
     }
 
+    function handleSubmit(event){
+        event.preventDefault()
+        console.log("Form submitted: ", formData)
+    }
+
+    console.log(formData.favColor)
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input
                 type="text"
                 placeholder="First Name"
@@ -46,6 +56,70 @@ export default function Form() {
                 onChange={handleChange}
                 name="comments"
             />
+            <input 
+                type="checkbox"
+                id="isFriendly"
+                checked={formData.isFriendly}
+                onChange={handleChange}
+                name="isFriendly"
+            />
+            <label htmlFor="isFriendly">Are you a foe?</label>
+
+            <fieldset>
+                <legend>Current employment status</legend>
+                
+                <input 
+                    type="radio"
+                    id="unemployed"
+                    name="employment"
+                    value="unemployed"
+                    onChange={handleChange}
+                    checked={formData.employment === "unemployed"}
+                />
+                <label htmlFor="unemployed">Unemployed</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="part-time"
+                    name="employment"
+                    value="part-time"
+                    onChange={handleChange}
+                    checked={formData.employment === "part-time"}
+                />
+                <label htmlFor="part-time">Part-time</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="full-time"
+                    name="employment"
+                    value="full-time"
+                    onChange={handleChange}
+                    checked={formData.employment === "full-time"}
+                />
+                <label htmlFor="full-time">Full-time</label>
+                <br />
+                
+            </fieldset>
+
+            <label htmlFor="favColor">What is your favorite color? </label>
+            <select 
+                id="favColor"
+                value={formData.favColor}
+                name="favColor"
+                onChange={handleChange}
+            >
+                <option value="red">Red</option>
+                <option value="orange">Orange</option>
+                <option value="yellow">Yellow</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+                <option value="indigo">Indigo</option>
+                <option value="violet">Violet</option>
+            </select>
+
+            <button>Submit</button>
         </form>
     )
 }
