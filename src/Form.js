afb48e22fa73ee23b99489559cabb37f104d1d12
Alexandre F <alexandreffaria@me.com>
2022-04-10 17:14:48 -0300

    state as obj

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,40 @@
 import React from "react"
 
 export default function Form() {
+    const [formData, setFormData] = React.useState({
+        firstName: "",
+        lastName: "",
+        email: ""
+    })
+    console.log(formData)    
+   
+    function handleChange(event){
+
+        setFormData(prevFormData => ({
+            ...prevFormData,
+            [event.target.name]: event.target.value
+            }))
+    }
+
     return (
         <form>
             <input
                 type="text"
                 placeholder="First Name"
+                onChange={handleChange}
+                name="firstName"
+            />
+            <input
+                type="text"
+                placeholder="Last Name"
+                onChange={handleChange}
+                name="lastName"
+            />
+            <input
+                type="text"
+                placeholder="Email"
+                onChange={handleChange}
+                name="email"
             />
         </form>
     )
